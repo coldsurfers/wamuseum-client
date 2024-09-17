63c37e13381d17fb6fdd819c7922ad47177f99df
yungblud <killi8n@gmail.com>
2024-09-17 21:44:02 +0900

    feat: added file upload artist profile image

--- app/register-artist/page.tsx
+++ app/register-artist/page.tsx
@@ -1,20 +1,76 @@
 'use client'
 
 import InputWithLabel from '@/ui/InputWithLabel'
-import { Button, Spinner, palette } from '@coldsurfers/hotsurf'
-import { useState } from 'react'
+import { Button, Spinner, Text, palette } from '@coldsurfers/hotsurf'
+import { useCallback, useState } from 'react'
 import styled from 'styled-components'
 import { useRouter } from 'next/navigation'
+import AddButton from '@/ui/AddButton'
+import pickFile from '@/utils/pickFile'
+import { presign, uploadToPresignedURL } from '@/utils/fetcher'
 import useCreateArtist from './mutations/useCreateArtist'
+
+const HeadWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 10px;
+`
+
+const PosterThumbnail = styled.img`
+  width: 100%;
+  height: 500px;
+  border-radius: 8px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  box-shadow:
+    0 1px 3px rgba(0, 0, 0, 0.12),
+    0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  object-fit: contain;
+`
 
 const RegisterArtistPage = () => {
   const router = useRouter()
   const [artistName, setArtistName] = useState('')
+  const [uploadFileLoading, setUploadFileLoading] = useState(false)
+  const [artistProfileImageUrl, setArtistProfileImageUrl] = useState('')
   const [mutate, { loading }] = useCreateArtist({
     onCompleted: () => {
       router.push('/')
     },
   })
+
+  const getThumbnail = useCallback(async () => {
+    pickFile(async (e) => {
+      const { target } = e
+      if (!target) return
+      const filename = new Date().toISOString()
+      const { files } = target as any
+      setUploadFileLoading(true)
+      try {
+        const presignedData = await presign({
+          type: 'artist-profile-images',
+          filename,
+          filetype: 'image/*',
+        })
+        await uploadToPresignedURL({
+          data: presignedData,
+          file: files[0],
+        })
+        setArtistProfileImageUrl(
+          `${
+            process.env.NEXT_PUBLIC_S3_BUCKET_URL
+          }/billets/artist/profile-images/${encodeURIComponent(filename)}`
+        )
+      } catch (err) {
+        console.error(err)
+      } finally {
+        setUploadFileLoading(false)
+      }
+    })
+  }, [])
+
   return (
     <Wrapper>
       <Form>
@@ -23,6 +79,23 @@ const RegisterArtistPage = () => {
           value={artistName}
           onChangeText={setArtistName}
         />
+        <HeadWrapper>
+          <Text style={{ fontSize: 16 }}>아티스트 프로필 이미지</Text>
+          {artistProfileImageUrl ? (
+            <Button
+              style={{ width: 10, height: 10, marginLeft: 'auto' }}
+              text={'✘'}
+              onPress={() => {
+                setArtistProfileImageUrl('')
+              }}
+            />
+          ) : (
+            <AddButton onPress={getThumbnail} />
+          )}
+        </HeadWrapper>
+        {artistProfileImageUrl && (
+          <PosterThumbnail src={artistProfileImageUrl} />
+        )}
         <Button
           text={'등록하기'}
           style={{ marginTop: 10, backgroundColor: palette.black }}
@@ -37,7 +110,7 @@ const RegisterArtistPage = () => {
           }}
         />
       </Form>
-      {loading ? <Spinner /> : null}
+      {loading || uploadFileLoading ? <Spinner /> : null}
     </Wrapper>
   )
 }
