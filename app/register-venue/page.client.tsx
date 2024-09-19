9b189c0a91cead39b630ae886c67d069138afff0
yungblud <killi8n@gmail.com>
2024-09-19 23:11:16 +0900

    feat: implemented createVenueMutation

--- app/register-venue/page.client.tsx
+++ app/register-venue/page.client.tsx
@@ -1,12 +1,15 @@
 'use client'
 
-import { Text, TextInput } from '@coldsurfers/hotsurf'
+import { Spinner, Text, TextInput } from '@coldsurfers/hotsurf'
 import styled from 'styled-components'
 import { useMemo, useState } from 'react'
 import { useDebounce } from '@uidotdev/usehooks'
+import { useRouter } from 'next/navigation'
 import useSearchVenueQuery from './queries/useSearchVenueQuery'
+import useCreateVenueMutation from './mutations/useCreateVenueMutation'
 
 const RegisterVenueClientPage = () => {
+  const router = useRouter()
   const [keyword, setKeyword] = useState('')
   const debouncedKeyword = useDebounce(keyword, 350)
   const { data: searchedVenues } = useSearchVenueQuery({
@@ -14,6 +17,8 @@ const RegisterVenueClientPage = () => {
       keyword: debouncedKeyword,
     },
   })
+  const [mutateCreateVenue, { loading: loadingCreateVenue }] =
+    useCreateVenueMutation({})
 
   const searchResult = useMemo(() => {
     if (searchedVenues?.searchVenue.__typename === 'SearchedVenueList') {
@@ -31,15 +36,32 @@ const RegisterVenueClientPage = () => {
         style={{ width: 520 }}
       />
       {searchResult.map((result) => {
-        if (!result) {
+        if (!result || !result.y || !result.x || !result.place_name) {
           return null
         }
         return (
-          <div key={result.id}>
+          <div
+            key={result.id}
+            onClick={() => {
+              mutateCreateVenue({
+                variables: {
+                  input: {
+                    lat: +result.y!,
+                    lng: +result.x!,
+                    name: result.place_name!,
+                  },
+                },
+                onCompleted: () => {
+                  router.push('/')
+                },
+              })
+            }}
+          >
             <Text>{result.place_name}</Text>
           </div>
         )
       })}
+      {loadingCreateVenue ? <Spinner /> : null}
     </Wrapper>
   )
 }
