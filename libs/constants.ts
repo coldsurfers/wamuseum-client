e5277510cc241fdcd31d0353295c426c78e6cacf
yungblud <killi8n@gmail.com>
2024-10-09 18:59:29 +0900

    chore: modified api url

--- libs/constants.ts
+++ libs/constants.ts
@@ -1,12 +1,12 @@
 const IS_DEV = process.env.NODE_ENV === 'development'
 
-const PROD_API_HOST = 'https://api.wamuseum.coldsurf.io'
+const PROD_API_HOST = 'https://api.wa-museum.coldsurf.io'
 
 export const urls = {
   apolloServer: IS_DEV
-    ? 'http://localhost:3001/api/graphql'
+    ? 'http://localhost:3002/api/graphql'
     : `${PROD_API_HOST}/api/graphql`,
   fileUploadPresignedServer: IS_DEV
-    ? 'http://localhost:3001/api/presigned'
+    ? 'http://localhost:3002/api/presigned'
     : `${PROD_API_HOST}/api/presigned`,
 }
